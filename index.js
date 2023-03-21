95cce5b9565523e9824469ed809a26b135c701a1
Jesus Miranda <jesus.miranda@ingenierosmafur.com>
2023-03-21 10:07:04 -0600

    Se cambió a SHA256

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const crypto = require("crypto-js");
+const crypto = require("crypto-js/sha256");
 
 class CryptoHelper {
   constructor(secret) {
@@ -8,7 +8,7 @@ class CryptoHelper {
   encryptSingleValue = async (data) => {
     let textEncrypt = null;
     if (data) {
-      textEncrypt = crypto.AES.encrypt(data, this.secret).toString();
+      textEncrypt = crypto.SHA256.encrypt(data, this.secret).toString();
     }
     return textEncrypt;
   };
@@ -16,7 +16,7 @@ class CryptoHelper {
   decryptSingleValue = async (data) => {
     let textValue = null;
     if (data) {
-      let textDecrypted = crypto.AES.decrypt(data, this.secret);
+      let textDecrypted = crypto.SHA256.decrypt(data, this.secret);
       textValue = textDecrypted.toString(crypto.enc.Utf8);
     }
     return textValue;
@@ -25,7 +25,10 @@ class CryptoHelper {
   encryptJsonValue = async (object, key) => {
     let jsonKeyValueEncrypt = object;
     if (JSON.stringify(object) !== {}) {
-      let textEncrypted = crypto.AES.encrypt(object[key], this.secret).toString();
+      let textEncrypted = crypto.SHA256.encrypt(
+        object[key],
+        this.secret
+      ).toString();
       jsonKeyValueEncrypt[key] = textEncrypted;
     }
     return jsonKeyValueEncrypt;
@@ -34,7 +37,7 @@ class CryptoHelper {
   decryptJsonValue = async (object, key) => {
     let jsonKeyDecrypted = object;
     if (JSON.stringify(object) !== {}) {
-      let textDecrypted = crypto.AES.decrypt(object[key], this.secret);
+      let textDecrypted = crypto.SHA256.decrypt(object[key], this.secret);
       let textValue = textDecrypted.toString(crypto.enc.Utf8);
       jsonKeyDecrypted[key] = textValue;
     }
